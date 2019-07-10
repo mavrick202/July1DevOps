960fce75718db4b48b47231c0018e3ebb4b036b7
mavrick202 <mavrick202@gmail.com>
2019-07-10 11:04:39 +0530

    Added line and $*

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 print("Running this python file with Jenkins....!!!!")
 for i in range(1,20,1):
-	print('-'*i)
-	print('This is line no:',i)
+	print('$$$'*i)
+	print('Line no:',i)
 for j in range(20,0,-1):
-	print('-'*j)
-	print('This is line no:',j)
+	print('***'*j)
+	print('Line no:',j)
